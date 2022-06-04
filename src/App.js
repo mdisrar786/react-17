d038bf0e049b510d6b9be9121ad848b62d59b22f
israr <mdisrar164@gmail.com>
2022-06-04 17:59:07 +0530

    changed the initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React,{useState}from 'react'
 function App() {
-  const [data,setData]=useState("Anil")
+  const [data,setData]=useState("israr")
   return (
     <div className="App">
       <h1>Hooks in React {data}</h1>
